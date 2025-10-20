3aeb74206e6f2793193087d6f9f63bda27461e52
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2025-10-20 20:37:30 +0200

    First Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
