8b7566356bf856c20c084050de0e446eb0a0418b
Johannes (jotrorox) Müller <mail@jotrorox.com>
2026-06-09 01:20:39 +0200

    Autodeploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isUserOrOrgPages = repoName?.endsWith('.github.io')
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS && repoName && !isUserOrOrgPages ? `/${repoName}/` : '/',
   plugins: [
       svelte(),
       tailwindcss(),
