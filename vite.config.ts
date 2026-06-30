7efa76ca1299474e158d3d878031c20d54eb8669
Johannes (jotrorox) <mail@jotrorox.com>
2026-06-30 21:31:04 +0200

    Migrating to solidjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import solid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
-      svelte(),
+      solid(),
       tailwindcss(),
   ],
 })
